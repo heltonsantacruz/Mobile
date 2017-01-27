bb91eb2a006ec37368b53069ee0ac005f7255192
Helton Santa Cruz Souto <helton.souto@dataprev.gov.br>
2017-01-27 11:26:41 -0300

    Commit muido

--- Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -2,16 +2,10 @@ package br.com.alura.agenda;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import br.com.alura.agenda.dao.AlunoDao;
